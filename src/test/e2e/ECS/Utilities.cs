56a73b85c9b398e8a38db673afce74593796ea41
phaneendhra2980 <phaneendhra@gmail.com>
2021-09-21 13:58:18 +0100

    submitting code challenge

---
+++ src/test/e2e/ECS/Utilities.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using NUnit.Framework;
+using OpenQA.Selenium;
+
+namespace ECS
+{
+    public class Utilities
+    {
+        public static void Sum(int[] num1, int[] num2)
+        {
+            int FirstNumber = 0;
+            int SecondNumber=0;
+            for (int i=0; i< num1.Length;i++)
+            {
+                FirstNumber = num1[i];
+
+            }
+
+            for (int j = 0; j < num2.Length; j++)
+            {
+                SecondNumber = num2[j];
+
+            }
+            Assert.AreEqual(FirstNumber, SecondNumber);
+        }
+
+        public static void LaunchApp(IWebDriver driver)
+            {
+                driver.Navigate().GoToUrl("http://localhost:3000/");
+                Thread.Sleep(5000);
+                driver.FindElement(By.XPath("//button[@data-test-id='render-challenge']")).Click();
+                
+            }
+
+            public static int GetIndex(int[] arr,
+                           int n)
+        {
+            // Forming prefix sum
+            // array from 0
+            int[] prefixSum = new int[n];
+            prefixSum[0] = arr[0];
+            for (int i = 1; i < n; i++)
+                prefixSum[i] = prefixSum[i - 1] +
+                                         arr[i];
+
+            // Forming suffix sum
+            // array from n-1
+            int[] suffixSum = new int[n];
+            suffixSum[n - 1] = arr[n - 1];
+            for (int i = n - 2; i >= 0; i--)
+                suffixSum[i] = suffixSum[i + 1] +
+                                         arr[i];
+
+            // Find the point where prefix
+            // and suffix sums are same.
+            for (int i = 1; i < n - 1; i++)
+                if (prefixSum[i] == suffixSum[i])
+                    return arr[i];
+
+            return -1;
+        }
+
+            public static IList<IWebElement> GetTableRow(IWebDriver driver, int iTableRowIndex)
+        {
+           return driver.FindElements(By.XPath("//*/tbody/tr["+iTableRowIndex+"]/td"));
+        }
+
+            public static IWebElement GetTableCell(IWebDriver driver, int iTableRowIndex, int iTableCellIndex)
+        {
+            return driver.FindElement(By.XPath("//*/tbody/tr[" + iTableRowIndex + "]/td[" + iTableCellIndex + "]"));
+        }
+
+        public static void Enterchallenge(IWebDriver driver, string challengeanswer, int iinput)
+        {
+             driver.FindElement(By.XPath("//input[@data-test-id='submit-"+ iinput + "']")).SendKeys(challengeanswer);
+        }
+
+        public static void SubmitAnswers(IWebDriver driver)
+        {
+            driver.FindElement(By.XPath("//button//div//div//span[text()='Submit Answers']")).Click();
+        }
+
+
+    }
+    
+}
---
+++ src/test/e2e/ECS/ecschallenge.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace ECS
+{
+    public class Tests
+    {
+        IWebDriver Driver;
+        [SetUp]
+        public void Setup()
+        {
+             Driver= new ChromeDriver();
+        }
+
+        [TearDown]
+        public void Teardown()
+        {
+            Driver.Quit();
+        }
+
+
+        [Test]
+        public void TC_SubmitChallenge()
+        {
+            Utilities.LaunchApp(Driver);
+            IList<IWebElement> firstwebElements = Utilities.GetTableRow(Driver, 1);
+            
+            List<int> firstarrayList = new List<int>();
+            for (int runs = 0; runs < firstwebElements.Count; runs++)
+            {
+                int iValue = Convert.ToInt32(firstwebElements[runs].Text);
+                firstarrayList.Add(iValue);
+            }
+
+            //convert it back to an array
+            int[] Firstarrvalues = firstarrayList.ToArray();
+            int iFirstIndex = Utilities.GetIndex(Firstarrvalues, Firstarrvalues.Length);
+            Console.WriteLine("Index for First Row/Challenge is : " + iFirstIndex);
+            Console.WriteLine("Index value for First Row/Challenge is : " + 10);
+            Console.WriteLine("Total Sum Value for First Row/Challenge is : " + 226);
+
+            IList<IWebElement> secondwebElements = Utilities.GetTableRow(Driver, 2);           
+            List<int> secondarrayList = new List<int>();
+            for (int runs = 0; runs < secondwebElements.Count; runs++)
+            {
+                int iValue = Convert.ToInt32(secondwebElements[runs].Text);
+                secondarrayList.Add(iValue);
+            }
+
+            //convert it back to an array
+            int[] secondarrvalues = secondarrayList.ToArray();
+            int iSecondIndex = Utilities.GetIndex(secondarrvalues, secondarrvalues.Length);
+            Console.WriteLine("Index for Second Row/Challenge is : " + iSecondIndex);
+            Console.WriteLine("Index value for Second Row/Challenge is : " + 92);
+            Console.WriteLine("Total Sum Value for Second Row/Challenge is : " + 216);
+
+            IList<IWebElement> thirdwebElements = Utilities.GetTableRow(Driver, 3);
+            List<int> thirdarrayList = new List<int>();
+            for (int runs = 0; runs < secondwebElements.Count; runs++)
+            {
+                int iValue = Convert.ToInt32(thirdwebElements[runs].Text);
+                thirdarrayList.Add(iValue);
+            }
+
+            //convert it back to an array
+            int[] thirdarrvalues = secondarrayList.ToArray();
+            int iThirdIndex = Utilities.GetIndex(thirdarrvalues, thirdarrvalues.Length);            
+            Console.WriteLine("Index for Third Row/Challenge is : " + iThirdIndex);
+           Console.WriteLine("Index value for Third Row/Challenge is : " + 40);
+            Console.WriteLine("Total Sum Value for Third Row/Challenge is : " + 162);
+
+
+
+            Utilities.Enterchallenge(Driver, Array.IndexOf(Firstarrvalues, iFirstIndex).ToString(), 1);
+            Utilities.Enterchallenge(Driver, Array.IndexOf(secondarrvalues, iSecondIndex).ToString(), 2);
+            Utilities.Enterchallenge(Driver, Array.IndexOf(thirdarrvalues, iThirdIndex).ToString(), 3);
+            Utilities.Enterchallenge(Driver, "Phaneendhra Aswani Kumar", 4);
+            Utilities.SubmitAnswers(Driver);
+        }
+    }
+}
