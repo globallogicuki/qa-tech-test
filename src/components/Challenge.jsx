9d278147674aca45082a05516fd37701b967e6bc
Matthew Lowry <matthewlowry@hotmail.co.uk>
2021-10-21 16:08:23 +0100

    Update Challenge.jsx

--- src/components/Challenge.jsx
+++ src/components/Challenge.jsx
@@ -21,10 +21,10 @@ const Challenge = () => (
 
         <h3>Below are a number of rows with integers</h3>
 
-        <p> Your job is to use selenium to read the dom and create an array data structure for each of the rows.</p>
+        <p> Your job is to use a browser based testing tool to read the dom and create an array data structure for each of the rows.</p>
         <p>
           Once you have each array,
-          write a function that is able to return the index of the array where the sum of integers at the index on the left is equal to the sum of integers on the right.</p>
+          write some code that is able to return the index of the array where the sum of integers at the index on the left is equal to the sum of integers on the right.</p>
         
         <p>If there is no index return null</p>
           
